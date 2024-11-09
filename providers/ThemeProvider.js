e5df76e2494d92d4a236ee468ac2f05095929ce4
Anik Islam <aanikbsmrstucse@gmail.com>
2024-11-09 09:33:15 +0600

    feat: implement dark mode depend on device color schemas

--- providers/ThemeProvider.js
+++ providers/ThemeProvider.js
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ThemeContext } from "../contexts";
 
 const ThemeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  // Function to toggle dark mode manually
   const toggleDarkMode = () => {
     setIsDarkMode((prevState) => !prevState);
   };
+
+  // Set theme based on system preference on component mount
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Initial check
+    setIsDarkMode(mediaQuery.matches);
+
+    // Event listener for system theme changes
+    const handleChange = (e) => {
+      setIsDarkMode(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    // Clean up the event listener on component unmount
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
