f4fad3bd9bf205079979d28d98771169621918dc
Aniruddha Adhikary <932949+aniruddha-adhikary@users.noreply.github.com>
2024-11-11 19:26:43 +0800

    chore: remove repositories

--- composeApp/src/commonMain/kotlin/net/adhikary/mrtbuddy/repository/ScanRepository.kt
+++
@@ -1,9 +0,0 @@
-package net.adhikary.mrtbuddy.repository
-
-import net.adhikary.mrtbuddy.dao.ScanDao
-import net.adhikary.mrtbuddy.data.ScanEntity
-
-class ScanRepository(private val scanDao: ScanDao) {
-    suspend fun insertScan(scan: ScanEntity): Long = scanDao.insertScan(scan)
-    suspend fun getScansByCardIdm(cardIdm: String): List<ScanEntity> = scanDao.getScansByCardIdm(cardIdm)
-}
