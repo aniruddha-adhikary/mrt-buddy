169a0c19e6b76e4a2207dd0e72d43079e6e0272f
aaditx23 <adit.pranto@gmail.com>
2024-12-09 20:42:39 +0600

    Fix: Minor cleanup

--- composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/net/adhikary/mrtbuddy/MainViewController.kt
@@ -1,63 +1,6 @@
-//package net.adhikary.mrtbuddy
-//
-//import androidx.compose.ui.window.ComposeUIViewController
-//import io.github.aakira.napier.DebugAntilog
-//import io.github.aakira.napier.Napier
-//import kotlinx.cinterop.ExperimentalForeignApi
-//import net.adhikary.mrtbuddy.di.appModule
-//import net.adhikary.mrtbuddy.di.platformModule
-//import org.koin.core.context.startKoin
-//import org.koin.core.error.KoinApplicationAlreadyStartedException
-//
-//import platform.UIKit.UIApplication
-//import platform.UIKit.UIColor
-//import platform.UIKit.UIScreen
-//import platform.UIKit.UIStatusBarStyleDarkContent
-//import platform.UIKit.UIStatusBarStyleLightContent
-//import platform.UIKit.UIView
-//import platform.UIKit.setStatusBarStyle
-//
-//fun MainViewController() = ComposeUIViewController {
-//    try {
-//        // Initialize Koin
-//        startKoin {
-//            modules(appModule, platformModule)
-//        }
-//    } catch (e: KoinApplicationAlreadyStartedException) {
-//        // Koin already started, ignore
-//    }
-//
-//    if (isDebug) {
-//        Napier.base(DebugAntilog())
-//    }
-//
-//
-//    App(
-//        dynamicColor = false,
-//        statusBarStyle = { mode ->
-//            setStatusBarStyle(mode)
-//        }
-//    )
-//}
-//
-//fun setStatusBarStyle(isDarkMode: Boolean) {
-//    val statusBarStyle = if (isDarkMode) {
-//        UIStatusBarStyleDarkContent
-//    } else {
-//        UIStatusBarStyleLightContent
-//    }
-//
-//    UIApplication.sharedApplication.setStatusBarStyle(statusBarStyle)
-//
-//}
-//
-
-
 package net.adhikary.mrtbuddy
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.window.ComposeUIViewController
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
@@ -108,17 +51,13 @@ fun configureStatusBarAppearance(isDarkMode: Boolean, backgroundColor: UIColor) 
         }
         UIApplication.sharedApplication.setStatusBarStyle(statusBarStyle)
 
-        // Remove any previous status bar background views by tag
-
-
-
         // Create a view for the status bar background
         val screenWidth = UIScreen.mainScreen.bounds.useContents { size.width }
         val statusBarHeight = window.safeAreaInsets.useContents { top }
         val statusBarFrame = CGRectMake(0.0, 0.0, screenWidth, statusBarHeight)
         val statusBarView = UIView(frame = statusBarFrame)
         statusBarView.backgroundColor = backgroundColor
-        statusBarView.tag = 12345L // Ensure the tag is a Long type in Kotlin/Native
+        statusBarView.tag = 12345L
 
         // Add the new status bar background view
         window.addSubview(statusBarView)
